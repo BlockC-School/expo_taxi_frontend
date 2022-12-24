f24b19bfb0467e01599510923de63de45f2e1315
anurag-shukla8004 <anuragshukla8004@gmail.com>
2022-12-24 13:06:08 +0530

    login&signup

---
+++ screens/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Loing</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Login;
